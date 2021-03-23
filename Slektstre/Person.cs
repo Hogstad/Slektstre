46cf60f3f5e90d46a45bd59a35626fb7b504e582
Flemming Hogstad <flemming.hogstad@gmail.com>
2021-03-23 11:34:45 +0100

    Initial commit

---
+++ Slektstre/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Slektstre
+{
+    public class Person
+    {
+        public int? Id;
+        public string FirstName;
+        public string LastName;
+        public int? BirthYear;
+        public int? DeathYear;
+        public Person Father;
+        public Person Mother;
+        private string getContent(string pre, object value, string after = null)
+        {
+            return value == null ? "" : $"{pre}{value}{after ?? ""}";
+        }
+        public string GetDescription()
+        {
+            return getContent("", FirstName, " ") +
+                   getContent("", LastName, " ") +
+                   getContent("(Id=", Id, ")") +
+                   getContent(" Født: ", BirthYear, " ") +
+                   getContent("Død: ", DeathYear, " ") +
+                   getContent("Far: ", Father, "") +
+                   getContent(" Mor: ", Mother, "");
+        }
+        public override string ToString()
+        {
+            if (Id != null)
+            {
+                return $"{FirstName ?? ""} {LastName ?? ""}(Id={Id})".Trim();
+            }
+            else return $"{FirstName ?? ""} {LastName ?? ""}".Trim();
+        }
+    }
+}
---
+++ SlektstreTest/PersonTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using Slektstre;
+
+namespace SlektstreTest
+{
+    class PersonTest
+    {
+        [Test]
+        public void TestAllFields()
+        {
+            var p = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                LastName = "Nordmann",
+                BirthYear = 2000,
+                DeathYear = 3000,
+                Father = new Person() { Id = 23, FirstName = "Per" },
+                Mother = new Person() { Id = 29, FirstName = "Lise" },
+            };
+
+            var actualDescription = p.GetDescription();
+            var expectedDescription = "Ola Nordmann (Id=17) Født: 2000 Død: 3000 Far: Per (Id=23) Mor: Lise (Id=29)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
+        }
+        [Test]
+        public void TestNoFields()
+        {
+            var p = new Person
+            {
+                Id = 1,
+            };
+
+            var actualDescription = p.GetDescription();
+            var expectedDescription = "(Id=1)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
+        }
+
+        [Test]
+        public void Flemming()
+        {
+            var p = new Person
+            {
+                Id = 13,
+                FirstName = "Flemming",
+                LastName = "Hogstad",
+                BirthYear = 1989,
+                Father = new Person() { Id = 55, },
+                Mother = new Person() { Id = 44, },
+            };
+
+            var actualDescriptio = p.GetDescription();
+            var expectedDescriptio = "Flemming Hogstad (Id=13) Født: 1989 Far: (Id=55) Mor: (Id=44)";
+
+            Assert.AreEqual(actualDescriptio, expectedDescriptio);
+        }
+    }
+}
